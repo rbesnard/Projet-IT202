e80ace2a8f05e6c063e4dda320717914557fd081
Jiang Lun <lun.jiang@gmail.com>
2012-02-14 16:48:44 +0100

    add of thread.h

---
+++ src/thread.h
@@ -0,0 +1,35 @@
+#ifndef __THREAD_H__
+#define __THREAD_H__
+
+/* identifiant de thread
+ * NB: pourra être un entier au lieu d'un pointeur si ca vous arrange,
+ *     mais attention aux inconvénient des tableaux de threads
+ *     (consommation mémoire, cout d'allocation, ...).
+ */
+typedef void * thread_t;
+
+/* recuperer l'identifiant du thread courant.
+ */
+extern thread_t thread_self(void);
+
+/* creer un nouveau thread qui va exécuter la fonction func avec l'argument funcarg.
+ * renvoie 0 en cas de succès, -1 en cas d'erreur.
+ */
+extern int thread_create(thread_t *newthread, void *(*func)(void *), void *funcarg);
+
+/* passer la main à un autre thread.
+ */
+extern int thread_yield(void);
+
+/* attendre la fin d'exécution d'un thread.
+ * la valeur renvoyée par le thread est placée dans *retval.
+ * si retval est NULL, la valeur de retour est ignorée.
+ */
+extern int thread_join(thread_t thread, void **retval);
+
+/* terminer le thread courant en renvoyant la valeur de retour retval.
+ * cette fonction ne retourne jamais.
+ */
+extern void thread_exit(void *retval) __attribute__ ((__noreturn__));
+
+#endif /* __THREAD_H__ */
