abad3d6a3a45588483b8a5f8f9295478abfad31f
Quentin Lambert <qlambert@enseirb-matmeca.fr>
2012-03-13 17:37:31 +0100

    implementation de la gestion des listes

--- src/thread.c
+++ src/thread.c
@@ -1,5 +1,5 @@
 #include "thread.h"
-//#include <glib.h>
+#include <glib.h>
 #include <stdlib.h>
 #include <ucontext.h>
 
@@ -10,51 +10,58 @@
  *          Zombie
  */
 
+struct GList * ready_list=NULL;
+struct GList * zombie_list=NULL;
+
 struct thread {
     ucontext_t uc;
-    /*glib list sleeping*/
+    struct GList * sleeping_list;
     void *retval;
 };
 
 
 thread_t thread_self(void) {
-    GList head = g_list_first(ready_list);
-    return head->data;
+    return (thread_t)g_list_nth_data(ready_list, 0);
 }
 
 int thread_create(thread_t *newthread, void *(*func)(void *), void *funcarg) {
+    if (ready_list == NULL)
+	ready_list = g_list_alloc();
+    if (zombie_list == NULL)
+	zombie_list = g_list_alloc();
+
     if(getcontext(&newthread->uc) == -1)
 	return -1;
     newthread->uc.uc_stack.ss_size = 64*1024;
     newthread->uc.uc_stack.ss_sp = malloc(uc.uc_stack.ss_size);
     uc.uc_link = NULL;
-    
+
     if(makecontext(newthread->uc, (void (*)(void)) func, 1, funcarg) == -1)
 	return -1;
     ready_list = g_list_append(ready_list, newthread);
-    
+
     return 0;
 }
 
 
 int thread_yield(void) {
-    
-    thread_t next, current;
-    GList head = list_head(ready_list);
-    current = head->data;
+    thread_t next, current = g_list_nth_data(ready_list, 0);
+
     ready_list = g_list_remove(ready_list, current);
     ready_list = g_list_append(ready_list, current);
+
     next = list_head(ready_list);
     return swapcontext(&current->uc, &next->uc);
-    
 }
 
 int thread_join(thread_t thread, void **retval) {
-    thread_t next, current = list_head(ready_list);
-    //remove_head(ready_list);
-    //add_tail(thread->sleeping_list, t);
+    thread_t next, current = g_list_nth_data(ready_list, 0);
+
+    ready_list = g_list_remove(ready_list, current);
+    ready_list = g_list_append(thread->sleeping_list, current);
+
     next = list_head(ready_list);
-    
+
     if(swapcontext(&current->uc, &next->uc) == -1)
 	return -1;
 
@@ -62,14 +69,25 @@ int thread_join(thread_t thread, void **retval) {
     return 0;
 }
 
-void thread_exit(void *retval) {    
-    thread_t t;
 
-    //pour chaque thread t dans sleeping list de la tete
-    t->retval=retval;
-    //placer thread dans ready_list
-    
-    //placer tete ready dans zombie
+void wakeup_func(gpointer data, gpointer user_data) {
+    (thread_t)data->retval = user_data;
+    g_list_append(ready_list, data);
+}
 
-    setcontext
+
+void thread_exit(void *retval) {
+
+    thread_t head = g_list_nth_data(ready_list, 0);
+
+    g_list_foreach(head,
+		   wakeup_func,
+		   retval);
+
+    g_list_free(head->sleeping);
+
+    zombie_list = g_list_append(zombie_list, head);
+    ready_list = g_list_remove(ready_list, head);
+
+    setcontext(g_list_nth_data(ready_list, 0)->uc);
 }
