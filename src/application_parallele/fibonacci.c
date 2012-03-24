1e89cdf7367ee841dbe2aec81c43136a2f8b8c69
GEORGES Amandine <amgeorges@enseirb-matmeca.fr>
2012-03-24 21:19:00 +0100

    Implémentation finale de fibonacci

--- src/application_parallele/fibonacci.c
+++ src/application_parallele/fibonacci.c
@@ -2,24 +2,32 @@
 #include <assert.h>
 #include <stdio.h>
 
+int i = 0;
+
 int fibonacci(int n){
-    if (n == 1 || n == 2)
+    if (n == 1 || n == 2){
+	thread_exit(1);
 	return 1;
+    }
     else{
 	thread_t thread1, thread2;
 	void * retval1, *retval2;
 	int err;
 	int a = n-1;
 	int b = n-2;
-	err = thread_create(&thread1, (void * (*) (void *))fibonacci, (void *) &a);
+	err = thread_create(&thread1, (void * (*) (void *))fibonacci, (void *) a);
 	assert(!err);
-	err = thread_create(&thread2, (void * (*) (void *))fibonacci, (void *) &b);
+	i++;
+	err = thread_create(&thread2, (void * (*) (void *))fibonacci, (void *) b);
 	assert(!err);
 	err = thread_join(thread2, &retval2);
 	assert(!err);
 	err = thread_join(thread1, &retval1);
 	assert(!err);
-	return (int)*retval1+(int)*retval2;
+	int c = (int) retval1;
+	int d = (int) retval2;
+	thread_exit(c+d);
+	return c+d;
     }
 }
 
@@ -27,8 +35,8 @@ int main(){
     thread_t threadfibo;
     void * retval;
     int err;
-    int n = 1;
-    err = thread_create(&threadfibo, (void * (*) (void *))fibonacci,(void*) &n);
+    int n = 10;
+    err = thread_create(&threadfibo, (void * (*) (void *))fibonacci,(void*) n);
     assert(!err);
     err = thread_join(threadfibo, &retval);
     printf("fibonacci(%d)=%d\n",n,(int)retval);
